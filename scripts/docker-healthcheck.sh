a0ad506711b43f6b037c2d2e3c8d10ddec9858bb
Marvin Winkler <m@rvin.at>
2021-06-04 09:43:28 +0200

    fixed exit 2

--- scripts/docker-healthcheck.sh
+++ scripts/docker-healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-diff /etc/postfix/tls /tmp/tls || echo 'certs were updated - need container restart! - exit 2' && exit 2
+diff /etc/postfix/tls /tmp/tls || exit 2
 
 [[ $(ps aux | grep '[r]unsvdir\|[p]ostfix/master -s\|[d]ovecot -F\|[p]ickup' | wc -l) -ge '4' ]]
 exit $?
