665ce95ec74ef9098460806e5b5a287457bf70bb
Marvin Winkler <m@rvin.at>
2023-03-21 00:55:58 +0100

    small fix

--- get-version.sh
+++ get-version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 export IMG=$(docker build -q --pull --no-cache -t 'get-version' .)
 
-export POSTFIX_VERSION=$(docker run --rm -ti get-version apk list 2>/dev/null | grep '\[installed\]' | grep "postfix-[0-9]" | cut -d " " -f1 | sed 's/postfix-//g' | tr -d '\r')
-export DOVECOT_VERSION=$(docker run --rm -ti get-version apk list 2>/dev/null | grep '\[installed\]' | grep "dovecot-[0-9]" | cut -d " " -f1 | sed 's/dovecot-//g' | tr -d '\r')
-export ALPINE_VERSION=$(docker run --rm -ti get-version cat /etc/alpine-release | tail -n1 | tr -d '\r')
+export POSTFIX_VERSION=$(docker run --rm -t get-version apk list 2>/dev/null | grep '\[installed\]' | grep "postfix-[0-9]" | cut -d " " -f1 | sed 's/postfix-//g' | tr -d '\r')
+export DOVECOT_VERSION=$(docker run --rm -t get-version apk list 2>/dev/null | grep '\[installed\]' | grep "dovecot-[0-9]" | cut -d " " -f1 | sed 's/dovecot-//g' | tr -d '\r')
+export ALPINE_VERSION=$(docker run --rm -t get-version cat /etc/alpine-release | tail -n1 | tr -d '\r')
 [ -z "$ALPINE_VERSION" ] && exit 1
 
 export IMGTAG=$(echo "$1:a$ALPINE_VERSION-p$POSTFIX_VERSION-d$DOVECOT_VERSION")
